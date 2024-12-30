ccbe29df5d7f87c18ae45812a44c5df33a855797
marcel <marcel.phippen@proton.me>
2024-12-30 03:21:50 +0100

    add icon and gosper glider gun

--- makefile
+++ makefile
@@ -2,9 +2,9 @@
 # Makefile Options
 # ----------------------------
 
-NAME = gol
+NAME = GoL
 ICON = icon.png
-DESCRIPTION = "Conway's game of life"
+DESCRIPTION = "Conway's game of life (by PurpleProg)"
 COMPRESSED = NO
 
 CFLAGS = -Wall -Wextra -Oz
--- src/main.c
+++ src/main.c
@@ -33,32 +33,76 @@ int main(void)
 	int columns = GFX_LCD_WIDTH / TILE_SIZE;
 
 	bool **board = (bool **)malloc(rows * sizeof(bool *));
-	for (int i = 0; i < rows; i++)
-	{
+	for (int i = 0; i < rows; i++) {
 		board[i] = (bool *)malloc(columns * sizeof(bool));
-		for (int j = 0; j < columns; j++)
-		{
+		for (int j = 0; j < columns; j++) {
 			board[i][j] = false;
 		}
 	}
 
+	// test glider gun
 
-	// test fill planer
+	//     123456789ABCDEF123456789ABCDEF123456
+	//
+	// 1   ....................................
+	// 2   ........................O...........
+	// 3   ......................O.O...........
+	// 4   ............OO......OO............OO
+	// 5   ...........O...O....OO............OO
+	// 6   OO........O.....O...OO..............
+	// 7   OO........O...O.OO....O.O...........
+	// 8   ..........O.....O.......O...........
+	// 9   ...........O...O....................
+	// A   ............OO......................
 
-	//    1 2 3
-	// 1    X
-	// 2      X
-	// 3  X X X
+	board[2][25] = true;
 
-	board[3][1] = true;
-	board[3][2] = true;
-	board[3][3] = true;
-	board[2][3] = true;
-	board[1][2] = true;
+	board[3][23] = true;
+	board[3][25] = true;
+
+	board[4][13] = true;
+	board[4][14] = true;
+	board[4][21] = true;
+	board[4][22] = true;
+	board[4][35] = true;
+	board[4][36] = true;
+
+	board[5][12] = true;
+	board[5][16] = true;
+	board[5][21] = true;
+	board[5][22] = true;
+	board[5][35] = true;
+	board[5][36] = true;
+
+	board[6][1] = true;
+	board[6][2] = true;
+	board[6][11] = true;
+	board[6][17] = true;
+	board[6][21] = true;
+	board[6][22] = true;
+
+	board[7][1] = true;
+	board[7][2] = true;
+	board[7][11] = true;
+	board[7][15] = true;
+	board[7][17] = true;
+	board[7][18] = true;
+	board[7][23] = true;
+	board[7][25] = true;
+
+	board[8][11] = true;
+	board[8][17] = true;
+	board[8][25] = true;
+
+	board[9][12] = true;
+	board[9][16] = true;
+
+	board[10][13] = true;
+	board[10][14] = true;
 
 
 	bool *paused = (bool *)malloc(sizeof(bool));
-	*paused = false;
+	*paused = true;
 
 	// allocate a cursor
 	cursor_t *cursor = malloc(sizeof(cursor_t));
